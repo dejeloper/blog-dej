12b81848ad0894b64f72b1b563f2af7a207ac7ed
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-06 12:53:14 -0500

    Add initial consts

---
+++ src/consts.ts
@@ -0,0 +1,27 @@
+export const SITE_METADATA = {
+  theme: "system",
+  language: "es",
+  robots: "index, follow",
+};
+
+export const ITEMS_PER_PAGE = 5;
+
+export const NAVIGATION = [
+  { href: "/", title: "nav.home" },
+  { href: "/blog", title: "nav.blog" },
+  { href: "/tags", title: "nav.tags" },
+  { href: "/about", title: "nav.about" },
+] as const;
+
+export const POST_METADATA = {
+  defaultLayout: "column",
+  showFullWidthCover: false,
+  showCover: true,
+  showTags: true,
+  showDate: true,
+  showSummary: true,
+  showAuthors: true,
+  showRelatedPosts: true,
+  showTableOfContents: true,
+  showShareButtons: "both",
+};
