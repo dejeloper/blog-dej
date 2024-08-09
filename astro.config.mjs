fc169c35bacf5337f0110de2e7fa954ef8eeb769
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-09 10:00:37 -0500

    Implement Solidjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
 import { SITE_METADATA } from "./src/consts.ts";
 import metaTags from "astro-meta-tags";
 
@@ -10,5 +11,12 @@ import robotsTxt from "astro-robots-txt";
 export default defineConfig({
   prefetch: true,
   site: SITE_METADATA.siteUrl,
-  integrations: [mdx(), sitemap(), tailwind(), metaTags(), robotsTxt()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    solidJs(),
+    metaTags(),
+    robotsTxt(),
+  ],
 });
