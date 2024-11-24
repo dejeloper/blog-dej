169623abce3cd49a1106252a7ec937f655240e99
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-11-24 09:20:01 -0500

    feat: Add reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import solidJs from "@astrojs/solid-js";
 import { SITE_METADATA } from "./src/consts.ts";
 import metaTags from "astro-meta-tags";
 
+import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
+
 import robotsTxt from "astro-robots-txt";
 
 export default defineConfig({
@@ -19,4 +21,7 @@ export default defineConfig({
     metaTags(),
     robotsTxt(),
   ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
